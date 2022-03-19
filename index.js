a09635b944abd3cf2e3c74e0d12a38577627e011
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2022-03-19 16:01:26 +0530

    additional features

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
+app.use(express.static(__dirname + '/css'));
+
+
 const port = process.env.PORT || 3000
+
+const users = {}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -19,11 +25,21 @@ io.on('connection', (socket) => {
   });
 
 io.on('connection', (socket) => {
+
+  socket.on('login', name=> io.emit('chat message', `${name} joined the chat`))
+                          // Chat message
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    console.log('message: ' + msg); // to add given message on console
     io.emit('chat message', msg);
   });
+                           // Connection/disconnect message
+  socket.on('disconnect', function() {
+    io.emit('chat message', `some user disconnected`);
+ });
+
+ console.log(`socket id : ${socket.id}`)
 });
+
 
 server.listen(port, () => {
   console.log('listening on port:3000');
